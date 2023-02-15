789ba656791cdfdbdba0e8ca942d83da5e090163
Leo Cainzos <dlcainzos@gmail.com>
2023-02-15 11:24:36 -0300

    Primer escritura en el repositorio

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log("Hola mundo!");
+
+const navToggle = document.querySelector(".nav-toggle")
+const navMenu = document.querySelector(".nav-menu")
+
+navToggle.addEventListener("click", () => {
+	navMenu.classList.toggle("nav-menu_visible")
+})
+
+
+function openH(evt, HName) {
+  var i, herram, links;
+  herram = document.getElementsByClassName("herramientas");
+  for (i = 0; i < herram.length; i++) {
+    herram[i].style.display = "none";
+  }
+  links = document.getElementsByClassName("item-conocimiento");
+  for (i = 0; i < links.length; i++) {
+    links[i].className = links[i].className.replace(" active", "");
+  }
+  document.getElementById(HName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
